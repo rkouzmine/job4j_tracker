8dc3f7a92df7d02db63f36ec5b79ba9d0136b39a
rkouzmine <krrsea@gmail.com>
2023-11-06 17:52:17 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Computer {
+
+    private boolean multiMonitor;
+    private int ssd;
+    private String cpu;
+
+    public Computer() {
+    }
+
+    public Computer(boolean multiMonitor, int ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd + " GB");
+        System.out.println("Модель cpu: " + cpu);
+    }
+
+    public static void main(String[] args) {
+        Computer computer = new Computer(true, 500, "Intel Core i7 10700K");
+        Computer comp = new Computer(true, 250, "AMD Ryzen 5 3600");
+        computer.printInfo();
+        comp.printInfo();
+    }
+}
