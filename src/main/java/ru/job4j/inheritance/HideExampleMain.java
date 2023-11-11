220cbbf3e7cb6af7b830ddfe0e3cefb55cf30b50
rkouzmine <krrsea@gmail.com>
2023-11-12 00:36:04 +0300

    5. Сокрытие метода [#504921]

---
+++ src/main/java/ru/job4j/inheritance/HideExampleMain.java
@@ -0,0 +1,17 @@
+package ru.job4j.inheritance;
+
+public class HideExampleMain {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal cat = new Animal();
+        animal.instanceInvoke();
+        cat.instanceInvoke();
+        Animal.staticInvoke();
+        Cat.staticInvoke();
+        cat.staticInvoke();
+        Animal other = null;
+        other.staticInvoke();
+        Cat kitty = new Cat();
+        kitty.instanceInvoke();
+    }
+}
