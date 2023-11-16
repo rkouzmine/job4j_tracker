c38f0d6f9d52b73110bf3588633090f894a44e56
rkouzmine <krrsea@gmail.com>
2023-11-16 23:35:32 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,13 +56,4 @@ public class Tracker {
         }
         return rsl;
     }
-
-    public boolean replace(int id, Item item) {
-        int index = indexOf(id);
-        if (index != -1) {
-            item.setId(id);
-            items[index] = item;
-        }
-        return index != -1;
-    }
 }
