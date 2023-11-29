3f0883844a466a076ab6becb7d3776d43e56dbc9
rkouzmine <krrsea@gmail.com>
2023-11-29 23:58:12 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/ex/CountTest.java
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -3,10 +3,22 @@ package ru.job4j.ex;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CountTest {
+
     @Test
     public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Count.add(10, 2);
+                });
+        assertThat(exception.getMessage()).isEqualTo("Start should be less than finish.");
+    }
+
+    @Test
+    public void whenStart0Finish3Then3() {
         int start = 0;
         int finish = 3;
         int expected = 3;
