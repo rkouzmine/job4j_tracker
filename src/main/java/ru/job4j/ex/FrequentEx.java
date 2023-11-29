b36032d2374d8462abe35f6b8a50837e1d4fcc34
rkouzmine <krrsea@gmail.com>
2023-11-29 16:08:02 +0300

    0. Что такое исключение. [#3821]

---
+++ src/main/java/ru/job4j/ex/FrequentEx.java
@@ -0,0 +1,10 @@
+package ru.job4j.ex;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", "Amazon", "Ozon"};
+        for (int i = 0; i < shops.length; i++) {
+            System.out.println(shops[i]);
+        }
+    }
+}
