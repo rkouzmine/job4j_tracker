e5272d628a0f32f4860cea819fa7f0c771b98221
rkouzmine <krrsea@gmail.com>
2023-12-04 13:14:59 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -28,6 +28,9 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
