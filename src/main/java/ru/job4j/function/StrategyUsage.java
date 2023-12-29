dee71080b9b4afcbbee03dbece8329c34ce5cd9e
rkouzmine <krrsea@gmail.com>
2023-12-29 19:23:31 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/StrategyUsage.java
@@ -0,0 +1,69 @@
+package ru.job4j.function;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class StrategyUsage {
+    /**
+     * public boolean empty(String string) {
+     * return string.isEmpty();
+     * }
+     * public boolean startWith(String string, String prefix) {
+     * return string.startsWith(prefix);
+     * }
+     * public boolean contains(String string, String key) {
+     * return string.contains(key);
+     * }
+     * public boolean empty(String string) {
+     * return check(str -> str.isEmpty(), string);
+     * }
+     * public boolean startWith(String string, String prefix) {
+     * return check(str -> str.startsWith(pref), string);
+     * }
+     * public boolean contains(String string, String key) {
+     * return check(str -> str.contains(key), string);
+     * }
+     */
+
+    public boolean check(Predicate<String> predicate, String string) {
+        return predicate.test(string);
+    }
+
+    public String transform(Function<String, String> function, String string) {
+        return function.apply(string);
+    }
+
+    public static void main(String[] args) {
+        StrategyUsage usage = new StrategyUsage();
+        System.out.println(
+                "Результат работы: " + usage.check(
+                        string -> string.isEmpty(), ""
+                )
+        );
+        System.out.println(
+                "Результат работы: " + usage.check(
+                        string -> string.startsWith("Fun"), "Functional interface"
+                )
+        );
+        System.out.println("Результат работы: " + usage.check(
+                string -> string.contains("rn"), "Surname Name"
+        ));
+
+        System.out.println(
+                "Строка после преобразования: " + usage.transform(
+                        string -> string.toUpperCase(), "aBCdEfghKLmnpRstU"
+                )
+        );
+        System.out.println(
+                usage.transform(
+                        string -> string.concat(
+                                "работает корректно"), "Строка после преобразования "
+                )
+        );
+        System.out.println(
+                "Строка после преобразования: " + usage.transform(
+                        string -> string.trim(), "    aBC dEfghK Lmnp RstU        "
+                )
+        );
+    }
+}
