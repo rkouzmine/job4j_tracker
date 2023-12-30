9224677f1760b975101598c67c685999ee0344a2
rkouzmine <krrsea@gmail.com>
2023-12-30 22:43:33 +0300

    4. Ссылки на конструкторы [#504922]

---
+++ src/main/java/ru/job4j/lambda/Model.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+public class Model {
+    private String name;
+
+    Model() {
+    }
+
+    public Model(String str) {
+        name = str;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
