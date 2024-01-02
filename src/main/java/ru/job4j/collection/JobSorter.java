b9bd36606d7962e2d9c855d04dd9969c21006575
rkouzmine <krrsea@gmail.com>
2024-01-02 19:57:32 +0300

    7. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -23,6 +23,23 @@ public class JobSorter {
                 .thenComparing(new JobDescByPriority());
         Collections.sort(jobs, combine);
         System.out.println();
+
         System.out.println(jobs);
+        System.out.println();
+
+        List<Job> jobList = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        jobs.sort(new JobDescByName().thenComparing(new JobDescByPriority()));
+        System.out.println(jobList);
+
+        Comparator<Job> compareName = Comparator.comparing(Job::getName);
+        Comparator<Job> comparePriority = Comparator.comparingInt(Job::getPriority);
+        Comparator<Job> jobCombine = compareName.thenComparing(comparePriority);
+
+        jobList.sort(jobCombine);
     }
 }
