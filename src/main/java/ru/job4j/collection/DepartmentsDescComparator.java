94fe01db7bdc08b84be31ee5060ccd63d20d37f9
rkouzmine <krrsea@gmail.com>
2024-01-05 23:15:40 +0300

    3. Отсортировать департаменты [#34352 #470533]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -8,9 +8,7 @@ public class DepartmentsDescComparator implements Comparator<String> {
         String[] strOne = left.split("/");
         String[] strTwo = right.split("/");
 
-        if (strTwo[0].compareTo(strOne[0]) != 0) {
-            return right.compareTo(left);
-        }
-        return left.compareTo(right);
+        int result = strTwo[0].compareTo(strOne[0]);
+        return result != 0 ? result : left.compareTo(right);
     }
 }
