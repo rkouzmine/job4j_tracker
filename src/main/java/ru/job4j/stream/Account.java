94684b549fab582dae45bfbaf4c57fc96d0a9811
rkouzmine <krrsea@gmail.com>
2024-01-10 12:29:19 +0300

    17. Путь от циклов к Stream API. [#24260]

---
+++ src/main/java/ru/job4j/stream/Account.java
@@ -0,0 +1,3 @@
+package ru.job4j.stream;
+
+public record Account(String requisite, double balance) { }
---
+++ src/main/java/ru/job4j/stream/User.java
@@ -0,0 +1,3 @@
+package ru.job4j.stream;
+
+public record User(String passport, String userName) { }
