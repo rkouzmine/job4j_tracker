121f28b8d3b5ded10ee31d3f0811fc73e7d638fc
rkouzmine <krrsea@gmail.com>
2024-02-01 16:32:31 +0300

    Способы сортировки. Сортировка List. Метод Collections.sort()

---
+++ src/test/java/ru/job4j/exercises/comparator/CollectionsSortTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.exercises.comparator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CollectionsSortTest {
+
+    @Test
+    public void test() {
+        List<String> input = Arrays.asList("c", "b", "a");
+        List<String> expect = Arrays.asList("a", "b", "c");
+        List<String> out = CollectionsSort.sort(input);
+        assertThat(expect).isEqualTo(out);
+    }
+}
