ba3e98ee1507b8529cb2a1f783b31cb5af82359d
rkouzmine <krrsea@gmail.com>
2024-02-05 01:17:41 +0300

    Добавление списка в другой список

---
+++ src/test/java/ru/job4j/exercises/list/AddAllElementsListTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.exercises.list;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AddAllElementsListTest {
+
+    @Test
+    public void containsElement() {
+        List<String> left = new ArrayList<>();
+        left.add("first");
+        left.add("second");
+        left.add("third");
+        List<String> right = new ArrayList<>();
+        right.add("fourth");
+        right.add("second");
+        right.add("fifth");
+        String check = "second";
+        int result = AddAllElementsList.containsElement(left, right, check);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void containsElementInFirstList() {
+        List<String> left = new ArrayList<>();
+        left.add("first");
+        left.add("second");
+        left.add("third");
+        List<String> right = new ArrayList<>();
+        right.add("fourth");
+        right.add("fifth");
+        String check = "second";
+        int result = AddAllElementsList.containsElement(left, right, check);
+        assertThat(result).isEqualTo(1);
+    }
+}
