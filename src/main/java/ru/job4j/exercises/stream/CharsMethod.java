bf8666846321f51e6813a6832ec553578cdfe1c2
rkouzmine <krrsea@gmail.com>
2024-02-19 15:39:46 +0300

    Промежуточные операции. Метод mapToObj(). Стрим из символов строки

---
+++ src/main/java/ru/job4j/exercises/stream/CharsMethod.java
@@ -0,0 +1,12 @@
+package ru.job4j.exercises.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CharsMethod {
+    public static List<Character> symbols(String string) {
+        return string.chars()
+                .mapToObj(n -> (char) n)
+                .collect(Collectors.toList());
+    }
+}
