c2d12d3a3e7dfffefd8258b8507597868cd145a5
rkouzmine <krrsea@gmail.com>
2024-02-20 16:45:12 +0300

    Промежуточные операции. Метод filter(). Фильтрация объектов

--- src/main/java/ru/job4j/exercises/stream/FilterObject.java
+++ src/main/java/ru/job4j/exercises/stream/FilterObject.java
@@ -1,5 +1,6 @@
 package ru.job4j.exercises.stream;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,6 +24,14 @@ public class FilterObject {
         public int getAge() {
             return age;
         }
+
+        @Override
+        public String toString() {
+            return "User{"
+                    + "name='" + name + '\''
+                    + ", age=" + age
+                    + '}';
+        }
     }
 
     public static List<User> filter(List<User> users) {
@@ -30,5 +39,18 @@ public class FilterObject {
                 .filter(n -> n.getName().startsWith("B"))
                 .filter(n -> n.getAge() > 18)
                 .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("Bob", 20));
+        users.add(new User("Pavel", 18));
+        users.add(new User("Maria", 16));
+        users.add(new User("Alex", 21));
+        users.add(new User("Bobby", 25));
+        users.add(new User("Tom", 43));
+
+        System.out.println(filter(users));
     }
 }
