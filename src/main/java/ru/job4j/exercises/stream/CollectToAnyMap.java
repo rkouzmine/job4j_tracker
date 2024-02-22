60492d1224dc4707d0b5c2d95c7c72e89e7b077e
rkouzmine <krrsea@gmail.com>
2024-02-22 23:08:59 +0300

    Сборка элементов. Сборка в произвольную реализацию Map

---
+++ src/main/java/ru/job4j/exercises/stream/CollectToAnyMap.java
@@ -0,0 +1,13 @@
+package ru.job4j.exercises.stream;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectToAnyMap {
+    public static Map<Integer, Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toMap(
+                k -> k, v -> (int) Math.pow(v, 2), (prev, curr) -> prev, LinkedHashMap::new));
+    }
+}
