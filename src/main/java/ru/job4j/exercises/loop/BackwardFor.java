5ac4342a03ef552cb5ba320da6f0b6e8e7dce368
rkouzmine <krrsea@gmail.com>
2024-02-25 00:28:31 +0300

    Вывести на консоль числа от M до N

---
+++ src/main/java/ru/job4j/exercises/loop/BackwardFor.java
@@ -0,0 +1,9 @@
+package ru.job4j.exercises.loop;
+
+public class BackwardFor {
+    public static void out(int m, int n) {
+        for (int i = m; i > n; i--) {
+            System.out.println(i);
+        }
+    }
+}
