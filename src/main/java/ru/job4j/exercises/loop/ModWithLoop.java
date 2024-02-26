f0bcae13cf3bfb5405d8765833410c82be925e7a
rkouzmine <krrsea@gmail.com>
2024-02-27 00:38:09 +0300

    mod через while

--- src/main/java/ru/job4j/exercises/loop/ModWithLoop.java
+++ src/main/java/ru/job4j/exercises/loop/ModWithLoop.java
@@ -2,6 +2,14 @@ package ru.job4j.exercises.loop;
 
 public class ModWithLoop {
     public static int mod(int n, int d) {
-        return Math.floorMod(n, d);
+        int rsl = 0;
+        while (n >= d + rsl) {
+            rsl += d;
+        }
+        return n - rsl;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mod(8, 3));
     }
 }
