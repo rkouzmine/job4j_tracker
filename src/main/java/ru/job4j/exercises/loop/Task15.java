5ca8a366ee54f42211993a626b1efa95da0144ab
rkouzmine <krrsea@gmail.com>
2024-02-27 15:51:57 +0300

    Вывод положительных трехзначных чисел в диапазоне

---
+++ src/main/java/ru/job4j/exercises/loop/Task15.java
@@ -0,0 +1,24 @@
+package ru.job4j.exercises.loop;
+
+import java.util.StringJoiner;
+
+public class Task15 {
+    public static void loop(int a, int b) {
+        String ln = System.lineSeparator();
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        for (int i = b; i > a; i--) {
+            if (i >= 100) {
+                stringJoiner.add(String.valueOf(i));
+            }
+        }
+        System.out.print(
+                "Старт" + ln
+                        + stringJoiner + ln
+                        + "Финиш" + ln
+        );
+    }
+
+    public static void main(String[] args) {
+        loop(16, 104);
+    }
+}
