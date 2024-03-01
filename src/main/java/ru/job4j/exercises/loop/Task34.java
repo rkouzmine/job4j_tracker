6228129a518dc0616787c0c4dc56fe649c639b60
rkouzmine <krrsea@gmail.com>
2024-03-02 00:35:55 +0300

    Вывести нечетные числа

---
+++ src/main/java/ru/job4j/exercises/loop/Task34.java
@@ -0,0 +1,25 @@
+package ru.job4j.exercises.loop;
+
+import java.util.StringJoiner;
+
+public class Task34 {
+    public static void loop(int num) {
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        int count = 0;
+        for (int i = num + 1; ; i++) {
+            if (i % 2 == 1) {
+                count++;
+                stringJoiner.add(String.valueOf(i));
+            } else if (count == 5) {
+                break;
+            }
+        }
+        System.out.println(stringJoiner);
+    }
+
+    public static void main(String[] args) {
+        loop(5);
+        loop(9);
+        loop(20);
+    }
+}
