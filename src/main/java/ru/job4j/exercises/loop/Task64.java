d6d73327daf6725606f68274bce67bc5df4a0762
rkouzmine <krrsea@gmail.com>
2024-03-09 20:19:05 +0300

    Дни рождения по месяцам

--- src/main/java/ru/job4j/exercises/loop/Task64.java
+++ src/main/java/ru/job4j/exercises/loop/Task64.java
@@ -9,28 +9,11 @@ public class Task64 {
 
         for (Integer num : months) {
             switch (num) {
-                case 12:
-                case 1:
-                case 2:
-                    winter++;
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    spring++;
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    summer++;
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    fall++;
-                    break;
-                default:
-                    System.out.println("Not a Month");
+                case 12, 1, 2 -> winter++;
+                case 3, 4, 5 -> spring++;
+                case 6, 7, 8 -> summer++;
+                case 9, 10, 11 -> fall++;
+                default -> System.out.println("Not a Month");
             }
         }
         System.out.println(
