efa713a7378dd3ec0f12bfe8af425a5672b055cd
rkouzmine <krrsea@gmail.com>
2024-03-23 00:07:38 +0300

    Объединить 3 массива

--- src/main/java/ru/job4j/exercises/array/UnionThreeArrays.java
+++ src/main/java/ru/job4j/exercises/array/UnionThreeArrays.java
@@ -8,7 +8,6 @@ public class UnionThreeArrays {
         int index = 2;
         result[0] = left[0];
         result[result.length - 1] = left[left.length - 1];
-
         for (int i = 1; i < middle.length; i += 2) {
             result[index] = middle[i];
             index += 2;
