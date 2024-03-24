95026cff61677217b66f14e845df7a56fb05bc38
rkouzmine <krrsea@gmail.com>
2024-03-24 23:50:18 +0300

    Пошаговое увеличение числа

---
+++ src/main/java/ru/job4j/exercises/type/Task15.java
@@ -0,0 +1,11 @@
+package ru.job4j.exercises.type;
+
+public class Task15 {
+    public static String increaseBy2(int number) {
+        int a, b, c;
+        a = number + 2;
+        b = a + 2;
+        c = b + 2;
+        return a + ", " + b + ", " + c;
+    }
+}
