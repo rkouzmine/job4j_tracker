6b534cb3298a9c25d8f9875f57ed2de3c30e7bb6
rkouzmine <krrsea@gmail.com>
2024-03-25 16:00:49 +0300

    Посчитать сумму цифр двузначного числа

---
+++ src/main/java/ru/job4j/exercises/type/Task28.java
@@ -0,0 +1,10 @@
+package ru.job4j.exercises.type;
+
+public class Task28 {
+    public static void numSum(int number) {
+        int num1, num2;
+        num1 = number % 10;
+        num2 = number / 10;
+        System.out.println(num2 + num1);
+    }
+}
