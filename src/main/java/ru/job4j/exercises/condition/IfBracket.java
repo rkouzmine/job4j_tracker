ff556f6445549d3560fd20c7740a6f239aac008c
rkouzmine <krrsea@gmail.com>
2024-03-31 14:41:15 +0300

    If. Забытые {}

---
+++ src/main/java/ru/job4j/exercises/condition/IfBracket.java
@@ -0,0 +1,18 @@
+package ru.job4j.exercises.condition;
+
+public class IfBracket {
+    public static void showA() {
+        System.out.println("A");
+    }
+
+    public static void showB() {
+        System.out.println("B");
+    }
+
+    public static void show(int num) {
+        if (num > 1) {
+            showA();
+            showB();
+        }
+    }
+}
