5f3635c5db6a47826b2f76fbf88dfa4f89374571
rkouzmine <krrsea@gmail.com>
2024-04-04 10:21:58 +0300

    Проверка на соответствие конкретному числу

---
+++ src/main/java/ru/job4j/exercises/condition/Task16.java
@@ -0,0 +1,7 @@
+package ru.job4j.exercises.condition;
+
+public class Task16 {
+    public static void zeroCheck(int number) {
+        System.out.println(number == 0 ? "Zero" : "Not zero");
+    }
+}
