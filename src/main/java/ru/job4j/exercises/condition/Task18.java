a32f1d578e5981eade45c22f6bfab84f0ef88b15
rkouzmine <krrsea@gmail.com>
2024-04-04 10:29:09 +0300

    Выполнить операции с числом при определенном условии

---
+++ src/main/java/ru/job4j/exercises/condition/Task18.java
@@ -0,0 +1,11 @@
+package ru.job4j.exercises.condition;
+
+public class Task18 {
+    public static void numDecrease(int number) {
+        if (number >= 100) {
+            System.out.println(number - 1);
+        } else {
+            System.out.println(number);
+        }
+    }
+}
