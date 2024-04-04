772ed71214c11d5032b30c9fdd7c1dedbd12183c
rkouzmine <krrsea@gmail.com>
2024-04-04 10:19:28 +0300

    Увеличить число вдвое

---
+++ src/main/java/ru/job4j/exercises/condition/Task15.java
@@ -0,0 +1,7 @@
+package ru.job4j.exercises.condition;
+
+public class Task15 {
+    public static void multiplyByTwo(int number) {
+        System.out.println(number > 0 ? number * 2 : number);
+    }
+}
