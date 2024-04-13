37e3a791c962481be67385f4a48c7e750900204f
rkouzmine <krrsea@gmail.com>
2024-04-13 15:56:55 +0300

    Равенство двух чисел

---
+++ src/main/java/ru/job4j/exercises/condition/Task26.java
@@ -0,0 +1,7 @@
+package ru.job4j.exercises.condition;
+
+public class Task26 {
+    public static void isEqual(int num1, int num2) {
+        System.out.println(num1 == num2 ? "Да" : "Нет");
+    }
+}
