e99280d25c45266a5607d8bc27ba924f06ca6b38
rkouzmine <krrsea@gmail.com>
2024-04-13 16:01:45 +0300

    Ученики и стулья

---
+++ src/main/java/ru/job4j/exercises/condition/Task27.java
@@ -0,0 +1,7 @@
+package ru.job4j.exercises.condition;
+
+public class Task27 {
+    public static void isEnough(int pupils, int chairs) {
+        System.out.println(pupils <= chairs ? "Да" : "Нет");
+    }
+}
