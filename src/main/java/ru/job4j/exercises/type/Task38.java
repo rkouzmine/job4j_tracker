cd2503dfe0280b69aaf7c80577c7fd8119b9683b
rkouzmine <krrsea@gmail.com>
2024-05-16 23:19:01 +0300

    "Линейный сдвиг влево" четырехзначного числа

--- src/main/java/ru/job4j/exercises/type/Task38.java
+++ src/main/java/ru/job4j/exercises/type/Task38.java
@@ -2,23 +2,27 @@ package ru.job4j.exercises.type;
 
 public class Task38 {
     public static void leftShift(int number) {
-        int num1 = number % 10;
-        number /= 10;
-        int num2 = number % 10;
-        number /= 10;
-        int num3 = number % 10;
+        if (number >= 1000 && number <= 9999) {
+            int num1 = number % 10;
+            number /= 10;
+            int num2 = number % 10;
+            number /= 10;
+            int num3 = number % 10;
 
-        StringBuilder result = new StringBuilder();
-        if (num3 != 0) {
-            result.append(num3).append(num2).append(num1).append(0);
-        } else if (num2 != 0) {
-            result.append(num2).append(num1).append(0);
-        } else if (num1 != 0) {
-            result.append(num1).append(0);
+            StringBuilder result = new StringBuilder();
+            if (num3 != 0) {
+                result.append(num3).append(num2).append(num1).append(0);
+            } else if (num2 != 0) {
+                result.append(num2).append(num1).append(0);
+            } else if (num1 != 0) {
+                result.append(num1).append(0);
+            } else {
+                result.append(0);
+            }
+            System.out.println(result);
         } else {
-            result.append(0);
+            System.out.println("Введите четырехзначное положительное число");
         }
-        System.out.println(result);
     }
 
     public static void main(String[] args) {
@@ -29,4 +33,3 @@ public class Task38 {
         leftShift(1000); //0
     }
 }
-
