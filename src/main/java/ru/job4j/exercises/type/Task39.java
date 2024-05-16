0ce4303a671cb937d4ba67aff3bb181525859549
rkouzmine <krrsea@gmail.com>
2024-05-16 22:52:22 +0300

    "Сдвиг влево по кругу" трехзначного числа

--- src/main/java/ru/job4j/exercises/type/Task39.java
+++ src/main/java/ru/job4j/exercises/type/Task39.java
@@ -2,20 +2,24 @@ package ru.job4j.exercises.type;
 
 public class Task39 {
     public static void leftCircleShift(int number) {
-        int lastDigit = number % 10;
-        number /= 10;
-        int middleDigit = number % 10;
-        number /= 10;
-        int firstDigit = number % 10;
+        if (number >= 100 && number <= 999) {
+            int lastDigit = number % 10;
+            number /= 10;
+            int middleDigit = number % 10;
+            number /= 10;
+            int firstDigit = number % 10;
 
-        StringBuilder result = new StringBuilder();
-        if (middleDigit != 0) {
-            result.append(middleDigit).append(lastDigit).append(firstDigit);
-        } else if (lastDigit != 0) {
-            result.append(lastDigit).append(firstDigit);
-        } else if (firstDigit != 0) {
-            result.append(firstDigit);
+            StringBuilder result = new StringBuilder();
+            if (middleDigit != 0) {
+                result.append(middleDigit).append(lastDigit).append(firstDigit);
+            } else if (lastDigit != 0) {
+                result.append(lastDigit).append(firstDigit);
+            } else if (firstDigit != 0) {
+                result.append(firstDigit);
+            }
+            System.out.println(result);
+        } else {
+            System.out.println("Введите трехзначное положительное число");
         }
-        System.out.println(result);
     }
 }
