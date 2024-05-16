d367f22e703f4438d0f67bd4333bb72e6a90f4e3
rkouzmine <krrsea@gmail.com>
2024-05-16 22:39:08 +0300

    "Сдвиг влево по кругу" трехзначного числа

--- src/main/java/ru/job4j/exercises/type/Task39.java
+++ src/main/java/ru/job4j/exercises/type/Task39.java
@@ -2,20 +2,20 @@ package ru.job4j.exercises.type;
 
 public class Task39 {
     public static void leftCircleShift(int number) {
-        int num1, num2, num3;
-
-        num1 = number % 10;
+        int lastDigit = number % 10;
         number /= 10;
-        num2 = number % 10;
+        int middleDigit = number % 10;
         number /= 10;
-        num3 = number % 10;
+        int firstDigit = number % 10;
 
-        if (num2 != 0) {
-            System.out.println(num2 + "" + num1 + "" + num3);
-        } else if (num2 == 0 && num1 != 0) {
-            System.out.println(num1 + "" + num3);
-        } else if (num2 == 0 && num1 == 0 && num3 != 0) {
-            System.out.println(num3);
+        StringBuilder result = new StringBuilder();
+        if (middleDigit != 0) {
+            result.append(middleDigit).append(lastDigit).append(firstDigit);
+        } else if (lastDigit != 0) {
+            result.append(lastDigit).append(firstDigit);
+        } else if (firstDigit != 0) {
+            result.append(firstDigit);
         }
+        System.out.println(result);
     }
 }
