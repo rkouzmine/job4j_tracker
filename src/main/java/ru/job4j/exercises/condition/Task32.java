077bcd6f1bef5eaf86ddb9b9f0009713bdc92c36
rkouzmine <krrsea@gmail.com>
2024-05-18 16:54:37 +0300

    Вывод разницы или произведения двух чисел в зависимости от условия

--- src/main/java/ru/job4j/exercises/condition/Task32.java
+++ src/main/java/ru/job4j/exercises/condition/Task32.java
@@ -1,13 +1,9 @@
 package ru.job4j.exercises.condition;
 
-import java.util.NoSuchElementException;
-
 public class Task32 {
     public static void calculate(int num1, int num2) {
-        if (num1 > num2) {
-            System.out.println(num1 - num2);
-        } else if (num1 < num2) {
-            System.out.println(num2 - num1);
+        if (num1 != num2) {
+            System.out.println(Math.abs(num1 - num2));
         } else {
             System.out.println(num1 * num2);
         }
