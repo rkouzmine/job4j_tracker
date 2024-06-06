63eaf37eeefdc348e598d91b957af2f22ec94a4c
rkouzmine <krrsea@gmail.com>
2024-06-06 22:38:12 +0300

    Вывести текстовую форму оценки

--- src/main/java/ru/job4j/exercises/condition/Task64.java
+++ src/main/java/ru/job4j/exercises/condition/Task64.java
@@ -2,22 +2,13 @@ package ru.job4j.exercises.condition;
 
 public class Task64 {
     public static void gradePrint(int number) {
-        switch (number) {
-            case 1:
-            case 2:
-                System.out.println("Плохо");
-                break;
-            case 3:
-                System.out.println("Удовлетворительно");
-                break;
-            case 4:
-                System.out.println("Хорошо");
-                break;
-            case 5:
-                System.out.println("Отлично");
-                break;
-            default:
-                System.out.println("Не соответствует");
-        }
+        String result = switch (number) {
+            case 1, 2 -> "Плохо";
+            case 3 -> "Удовлетворительно";
+            case 4 -> "Хорошо";
+            case 5 -> "Отлично";
+            default -> "Не соответствует";
+        };
+        System.out.println(result);
     }
 }
