b3ab6c2bd0fc70bad112044d5aacc41e4b53f8c6
rkouzmine <krrsea@gmail.com>
2024-08-03 23:21:59 +0300

    Индексы символов

---
+++ src/main/java/ru/job4j/exercises/map/Concordance.java
@@ -0,0 +1,21 @@
+package ru.job4j.exercises.map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Concordance {
+    public static Map<Character, List<Integer>> collectCharacters(String s) {
+        Map<Character, List<Integer>> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char ch = chars[i];
+            if (ch != ' ') {
+                map.putIfAbsent(ch, new ArrayList<>());
+                map.get(ch).add(i);
+            }
+        }
+        return map;
+    }
+}
