61b77d45bef29897a9e403bc0dcd5cfe647f4ffd
rkouzmine <krrsea@gmail.com>
2024-10-13 15:00:06 +0300

    Среднее арифметическое максимума и минимума серии чисел

--- src/main/java/ru/job4j/exercises/loop/Task98.java
+++ src/main/java/ru/job4j/exercises/loop/Task98.java
@@ -2,14 +2,11 @@ package ru.job4j.exercises.loop;
 
 public class Task98 {
     public static void loop(int[] num) {
-        int max = 0;
-        int min = Integer.MAX_VALUE;
+        int max = num[0];
+        int min = num[0];
         for (int i : num) {
-            if (i > max) {
-                max = i;
-            } else if (i < min) {
-                min = i;
-            }
+            max = Math.max(i, max);
+            min = Math.min(i, min);
         }
         int result = (max + min) / 2;
         System.out.println(result);
