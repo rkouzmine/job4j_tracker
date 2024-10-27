87110ca52c80d63bf2e1f35df1b7a48518ca5f22
rkouzmine <krrsea@gmail.com>
2024-10-27 17:28:49 +0300

    Подсчитать количество выведенных значений

--- src/main/java/ru/job4j/exercises/loop/Task107.java
+++ src/main/java/ru/job4j/exercises/loop/Task107.java
@@ -1,24 +1,16 @@
 package ru.job4j.exercises.loop;
 
-import java.util.StringJoiner;
-
 public class Task107 {
     public static void loop(int[] nums) {
-        StringJoiner sj = new StringJoiner("\n");
-//        sj.setEmptyValue("Значений: 0");
         int count = 0;
         for (int num : nums) {
             if (num == 999) {
                 break;
             }
-            sj.add(String.valueOf(num));
+            System.out.println(num);
             count++;
         }
-        if (sj.length() != 0) {
-            System.out.printf("%s%nЗначений: %d%n", sj, count);
-        } else {
-            System.out.println("Значений: " + count);
-        }
+        System.out.printf("Значений: %d%n", count);
     }
 
     public static void main(String[] args) {
