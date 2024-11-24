b583b8abf8a6b15b058a74bf832e750d3d45348e
rkouzmine <krrsea@gmail.com>
2024-11-24 22:17:18 +0300

    Вывести первую цифру числа

---
+++ src/main/java/ru/job4j/exercises/loop/Task127.java
@@ -0,0 +1,11 @@
+package ru.job4j.exercises.loop;
+
+public class Task127 {
+    public static void loop(int num) {
+        System.out.println(String.valueOf(num).charAt(0));
+    }
+
+    public static void main(String[] args) {
+        loop(145);
+    }
+}
