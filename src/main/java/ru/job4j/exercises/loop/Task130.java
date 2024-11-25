b85298f47859cd37e57239197d8f3bf477aa9eb9
rkouzmine <krrsea@gmail.com>
2024-11-25 16:07:59 +0300

    Подсчитать количество нулей в числе

--- src/main/java/ru/job4j/exercises/loop/Task130.java
+++ src/main/java/ru/job4j/exercises/loop/Task130.java
@@ -3,12 +3,12 @@ package ru.job4j.exercises.loop;
 public class Task130 {
     public static void loop(int num) {
         int count = 0;
-        char[] chars = String.valueOf(num).toCharArray();
-        for (char symbol : chars) {
-           String ch = String.valueOf(symbol);
-            if ("0".equals(ch)) {
+        int digit = num;
+        while (digit != 0) {
+            if (digit % 10 == 0) {
                 count++;
             }
+            digit /= 10;
         }
         System.out.println(count);
     }
